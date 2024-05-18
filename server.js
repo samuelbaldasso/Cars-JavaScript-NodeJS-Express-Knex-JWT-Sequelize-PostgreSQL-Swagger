f4ceccc9e562f7ac1ef30ff7fda1beafdcfdca4d
Samuel Baldasso <53982121+samuelbaldasso@users.noreply.github.com>
2024-05-17 21:12:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({error: 'Erro interno do servidor.'});
 });
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(3001, "0.0.0.0", () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
